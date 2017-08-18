bdc5321a6e7a00cccc9a3f3bb39ddb005d90da68
felipessalvatore <felipessalvador@googlemail.com>
2017-08-18 16:00:15 -0300

    Add score function

--- src/eval/metrics.py
+++ src/eval/metrics.py
@@ -1,5 +1,7 @@
 import pickle
 import heapq
+import sys
+import numpy as np
 
 
 def analogy(word1, word2, word3, word2index, embeddings):
@@ -39,3 +41,94 @@ def analogy(word1, word2, word3, word2index, embeddings):
         _, index = heapq._heappop_max(all_results)
         results.append(index2word[index])
     return results
+
+
+def score(word2index,
+          embeddings,
+          eval_path,
+          verbose=True,
+          raw=False):
+    """
+    Function to calculate the score of the embeddings given one
+    txt file of analogies "eval_path". A valid line is a line of the txt
+    such that every word is in the vocabulary of the embeddings. For each
+    valid line we calculate the top 10 closest words that fit the analogy for
+    the first, the second and the third words of the valid line. The score of
+    this line will be the position of the fourth word in this list (0 if it is
+    not in the list). Since the txt can have different categories this
+    function also returns a list 'results' with the different scores
+    per category.
+
+    :type word2index: dict
+    :type embeddings: np array
+    :type eval_path:str
+    :rtype final_score: float
+    :rtype results: list
+    """
+    old_score = 0
+    old_total = 0
+    old_cat = None
+    valid_tests = 0
+    total_lines = 0
+    all_cat_scores = []
+    all_cat_totals = []
+    all_cat = []
+    with open(eval_path) as inputfile:
+        for line in inputfile:
+            total_lines += 1
+            list_line = line.strip().split()
+            if list_line[0] == ":":
+                if verbose:
+                    print("\n" + line + "\n")
+                if old_cat is not None:
+                    all_cat.append(old_cat)
+                    all_cat_scores.append(old_score)
+                    if raw:
+                        all_cat_totals.append(old_total)
+                    else:
+                        all_cat_totals.append(old_total * 10)
+                    old_cat = list_line[1]
+                    old_score = 0
+                    old_total = 0
+                else:
+                    old_cat = list_line[1]
+                    old_score = 0
+                    old_total = 0
+            if all([word in word2index for word in list_line]):
+                current_score = 0
+                valid_tests += 1
+                old_total += 1
+                analogues = analogy(list_line[0],
+                                    list_line[1],
+                                    list_line[2],
+                                    word2index,
+                                    embeddings)[::-1]
+                if raw:
+                    if list_line[3] == analogues[9]:
+                        current_score = 1
+                else:
+                    if list_line[3] in analogues:
+                        current_score = analogues.index(list_line[3]) + 1
+                old_score += current_score
+                if verbose:
+                    sys.stdout.write('\rline:{}|cat:{}|score:{}'.format(total_lines,
+                                                                        old_cat,
+                                                                        old_score))
+                    sys.stdout.flush()
+    all_cat.append(old_cat)
+    all_cat_scores.append(old_score)
+    if raw:
+        all_cat_totals.append(old_total)
+    else:
+        all_cat_totals.append(old_total * 10)
+    results = [cat + ": {0:.1f}% ({1}/{2})".format((score/total)*100,
+                                                   score, total)
+               for (cat, score, total) in zip(all_cat,
+                                              all_cat_scores,
+                                              all_cat_totals) if total != 0]
+    if all_cat_totals == []:
+        final_score = 0
+        print("Every line has at least a word outside the vocabulary")
+    else:
+        final_score = np.sum(all_cat_scores) / np.sum(all_cat_totals)
+    return final_score, results
--- src/test/MetricsTest.py
+++ src/test/MetricsTest.py
@@ -9,7 +9,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 from utils import run_test, load_embeddings
-from eval.metrics import analogy
+from eval.metrics import analogy, score
 
 
 class MetricsTest(unittest.TestCase):
@@ -20,6 +20,7 @@ class MetricsTest(unittest.TestCase):
     def setUpClass(cls):
         cls.filename = os.path.join(parentdir, 'pickles', "toy.pickle")
         cls.embeddings, cls.word2index = load_embeddings(cls.filename)
+        cls.analogy_path = os.path.join(parentdir, 'analogies', "toy_ptbr.txt")
 
     def test_analogy(self):
         """
@@ -34,6 +35,18 @@ class MetricsTest(unittest.TestCase):
         self.assertEqual(similar_words[0], "ela",
                          msg="\nsimilar_words= \n {}".format(similar_words))
 
+    def test_score(self):
+        """
+        Function to test if all the sentences that are
+        being generated are valid tweets
+        """
+        my_score, result_list = score(MetricsTest.word2index,
+                                      MetricsTest.embeddings,
+                                      MetricsTest.analogy_path,
+                                      verbose=False)
+        my_score *= 100
+        self.assertEqual(my_score, 100,
+                         msg="\nresults = \n {}".format(result_list))
 
 if __name__ == "__main__":
     run_test(MetricsTest,
